051240e1acd9d1ed1bb94470c03a1a07617d5888
Slavar <slavaleo1991@gmail.com>
2017-03-07 17:22:50 +0200

    ver 1.0

---
+++ js/common.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	function heightDetect (){
+	$(".main_head").css("height",$(window).height());
+}
+heightDetect()
+$(window).resize(function(){
+heightDetect()  
+})
+
+$(".toggle_mnu, .menu_item").click(function() {
+  $(".sandwich").toggleClass("active");
+});	
+
+$(".toggle_mnu").click(function(){
+	if ($(".top_mnu").is(":visible")) {
+		$(".top_mnu").fadeOut(600);
+	} else {
+	$(".top_mnu").fadeIn(600)
+};
+	
+});
+})
+
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
+
+
+$(".test").click(function(){
+	$(".test2").fadeOut()
+})
